a56464bc07c1dd0ebc6e44b208e34c362a78cb12
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-02-20 20:26:23 +0300

    connect tiny-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,22 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 800,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png" alt="left"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png" alt="right"></button>',
-        responsive: [{
-            breakpoint: 992,
-            settings: {
-              dots: true,
-              arrows: false
-            }
-          }]
-    });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 800,
+//         // adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png" alt="left"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png" alt="right"></button>',
+//         responsive: [{
+//             breakpoint: 992,
+//             settings: {
+//               dots: true,
+//               arrows: false
+//             }
+//           }]
+//     });
+//   });
+
+var slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false
   });
