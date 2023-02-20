1d1d0281a7eb87e4478297aff432cc318dd89ef1
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-02-20 18:45:37 +0300

    add jpg file, deleted png

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 800,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png" alt="right"></button>'
+    });
+  });
