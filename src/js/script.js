14b712251625ba4d9c937381282cd457b1c0f2c0
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-02-20 18:57:55 +0300

    add responsive and dots to slider

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,13 @@ $(document).ready(function(){
         speed: 800,
         // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png" alt="left"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png" alt="right"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png" alt="right"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+              dots: true,
+              arrows: false
+            }
+          }]
     });
   });
