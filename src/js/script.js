0c5c07e4eaac958757daa1b060ed7c3cb3bb71e5
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-02-21 09:51:42 +0300

    deleted comment, add nav to slider, img in slider width 80%

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,15 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 800,
-//         // adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png" alt="left"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png" alt="right"></button>',
-//         responsive: [{
-//             breakpoint: 992,
-//             settings: {
-//               dots: true,
-//               arrows: false
-//             }
-//           }]
-//     });
-//   });
 
-var slider = tns({
+const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
-    autoplay: false
+    autoplay: false,
+    mouseDrag: true,
+    nav: false,
+    controlsText: [
+      '<img src="icons/left.png" alt="left">',
+      '<img src="icons/right.png" alt="right">'
+    ],
+
   });
 
-  const left = document.querySelector('[data-controls=prev]');
-  left.innerHTML=`
-  <img src="icons/left.png" alt="left">
-  `;
-
-  const right = document.querySelector('[data-controls=next]');
-  right.innerHTML=`
-  <img src="icons/right.png" alt="left">
-  `;
